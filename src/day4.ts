512ef20d6457803ffd74b9aa6cbc02a85fd287cb
nonota <furuu.code.works@gmail.com>
2026-02-21 09:07:55 +0900

    新機能追加

--- src/day4.ts
+++ src/day4.ts
@@ -1,55 +1,31 @@
-// type Reservation = {
-//   name: string;
-//   checkin: string;
-//   checkout: string;
-//   tel: string;
-//   people: number;
-// };
-
-// function getTodayString(): string {
-//   const today = new Date();
-//   const y = today.getFullYear();
-//   const m = String(today.getMonth() + 1).padStart(2, "0");
-//   const d = String(today.getDate()).padStart(2, "0");
-//   return `${y}-${m}-${d}`;
-// }
-
-// function getTodayCheckinGuests(reservations: Reservation[]): Reservation[] {
-//   const today = getTodayString();
-//   return reservations.filter((r) => r.checkin === today);
-// }
-
-// const today = getTodayString();
-// const reservations: Reservation[] = [
-//   { name: "山田太郎", checkin: today, checkout: "2025-02-22", tel: "090-1234-5678", people: 2 },
-//   { name: "佐藤花子", checkin: "2025-02-19", checkout: "2025-02-21", tel: "080-9876-5432", people: 1 },
-//   { name: "鈴木一郎", checkin: today, checkout: "2025-02-23", tel: "070-1111-2222", people: 3 },
-// ];
-
-// const todayGuests = getTodayCheckinGuests(reservations);
-// console.log("本日チェックインのお客様:", todayGuests);
-
 
 type Reservation = {
-	name: string;
-  checkin: string;
-  checkout: string;
-  tel: string;
-  people: number;
+    name: string;
+    checkin: string;
+    checkout: string;
+    tel: string;
+    people: number;
 };
 
 function getTodayString(): string {
     const today = new Date();
     const y = today.getFullYear();
     const m = String(today.getMonth() + 1).padStart(2,"0");
-    const d = String(today.getDay() + 1).padStart(2,"0");
-    return `${y}-${m}-${d}`
+    const d = String(today.getDate()).padStart(2,"0");
+    return `${y}-${m}-${d}`;
 };
 
 function getTodayCheckinGuests(reservations: Reservation[]): Reservation[] {
     const today = getTodayString();
     return reservations.filter((r) => r.checkin === today);
 };  
+
+function todayGuestPeopleTotal(): number {
+    const today = getTodayString();
+    return reservations
+        .filter((r) => r.checkin === today)
+        .reduce((sum,r) => r.people + sum ,0);
+};
 
 const today = getTodayString();
 const reservations: Reservation[] = [
@@ -58,5 +34,6 @@ const reservations: Reservation[] = [
   { name: "鈴木一郎", checkin: today, checkout: "2025-02-23", tel: "070-1111-2222", people: 3 },
 ];
 
-const todayGuests = getTodayCheckinGuests(reservations)
+const todayGuests = getTodayCheckinGuests(reservations);
 console.log("本日のチェックインのお客様は", todayGuests);
+console.log(todayGuestPeopleTotal);
